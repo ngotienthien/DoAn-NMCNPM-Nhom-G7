39fd33f32c1d852e695e7941a3a5b95d00d091d6
luongvinh <luongthevinh.dev@gmail.com>
2021-03-13 13:37:00 +0700

    show place, category sub in header, category

--- Source/models/category.model.js
+++ Source/models/category.model.js
@@ -3,17 +3,43 @@ const db = require("../utils/db");
 const TBL_category = "cate";
 const TBL_sub_category = "sub_categories";
 module.exports = {
+  allCate: function () {
+    return db.load(`
+    SELECT IDCategory, CategoryName
+    FROM cate 
+    WHERE status = 1`);
+  },
+  allCateNumber: function () {
+    return db.load(`
+    SELECT c.IDCategory, c.CategoryName, COUNT(c.IDCategory) Number
+    FROM cate c, article a
+    WHERE c.Status = 1
+    and c.IDCategory = a.IDCate
+    ORDER BY Number`);
+  },
+
+  allSubCate: function () {
+    //use show.route.js with get "/" (home header -> Điểm đến)
+    return db.load(
+      `SELECT IDSubCategory, IDCategory, SubCategoryName FROM sub_categories WHERE status = 1`
+    );
+  },
+
+  SingleSubCate: function (id) {
+    return db.load(
+      `
+      SELECT s.IDSubCategory, s.SubCategoryName
+      FROM sub_categories s
+      WHERE s.Status = 1
+      AND s.IDSubCategory = ${id}
+      `
+    );
+  },
+
+  ////////////////////
   Load: function () {
     return db.load(
       `select a.IDArticle id,t.TagName from article a join article_tag art on a.IDArticle = art.IDArticle join tags t on art.IDTag = t.IDTag where a.Status = 6`
-    );
-  },
-  allCate: function () {
-    return db.load(`SELECT IDCategory, CategoryName FROM cate WHERE status = 1`);
-  },
-  allSubCate: function () {
-    return db.load(
-      `SELECT IDSubCategory, IDCategory, SubCategoryName FROM sub_categories WHERE status = 1`
     );
   },
   LoadCate: function () {
@@ -49,11 +75,7 @@ module.exports = {
       `select IDCategory, CategoryName from ${TBL_category} where status = 1 and IDCategory=${id}`
     );
   },
-  SingleSubCate: function (id) {
-    return db.load(
-      `select sc.IDSubCategory,sc.IDCategory, sc.SubCategoryName,c.CategoryName from ${TBL_sub_category} sc join ${TBL_category} c on sc.IDCategory = c.IDCategory where sc.Status = 1 and sc.IDSubCategory=${id}`
-    );
-  },
+
   GetNameByID:async function(id){  
       const result = await db.load(`SELECT CategoryName FROM ${TBL_category} WHERE IDCategory = ${id}`);
       return result[0].CategoryName;
